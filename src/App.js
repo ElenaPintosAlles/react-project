79a34b7ada9f3a53c2d63016dbca27580568c667
ElenaPintosAlles <92037978+ElenaPintosAlles@users.noreply.github.com>
2022-04-10 18:49:12 +0200

    weather API city temp alerted

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hi everyone</h1>
-        <Weather />
+        <Weather city="London" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,15 @@
 import React from "react";
 import axios from "axios";
 
-export default function Weather() {
-  return <h1>Hi from Weather</h1>;
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The Weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=bcec07f4d24f0897e35b27235c1cbd67&units=metric`;
+  axios.get(apiURL).then(handleResponse);
+
+  return <h2>It's loading...</h2>;
 }
